0f9608bfdd848e91be4f28c08c558d5e6485c037
Victor Nouvellet <victor.nouvellet@gmail.com>
2020-01-31 19:10:33 +0100

    Add first test
    - Add first test
    - Add Kingfisher pod for image caching

--- QontoAssessmentTests/QontoAssessmentTests.swift
+++ QontoAssessmentTests/QontoAssessmentTests.swift
@@ -10,25 +10,28 @@ import XCTest
 @testable import QontoAssessment
 
 class QontoAssessmentTests: XCTestCase {
+    
+    var mainInteractor: MainInteractor!
+    let timeout = 1.0
 
     override func setUp() {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+        mainInteractor = MainInteractor()
     }
 
     override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        mainInteractor = nil
     }
 
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
+    func testCallbackUpdate() {
+        let expectation = XCTestExpectation(description: "default user list is nil")
+        
+        mainInteractor.callbackModelUpdate = { (viewModel) in
+            XCTAssertNil(viewModel.userList)
+            expectation.fulfill()
         }
+        mainInteractor.onViewDidLoad()
+        
+        wait(for: [expectation], timeout: timeout)
     }
 
 }
